44f4b846486161d291b3ff35f9bee4d3bf711e12
Gnani-is-Coding <gnanendragariminti@gmail.com>
2024-11-09 19:03:59 +0530

    add progress bar and logic for it

--- toast_notifications/toast.js
+++ toast_notifications/toast.js
@@ -18,10 +18,33 @@ const data = {
 }
 
 const btnsList = document.querySelectorAll(".btn");
+let width = 0;
+let timerId = null
+let toastDiv = undefined;
+function runTimer() {
+    return new Promise((resolve, reject) => {
+        timerId = setInterval(() => {
+            width += 4;
+            if (width > 100) {
+                clearInterval(timerId)
+                width = 0;
+                return resolve()
+            }
+            document.querySelector(".progress").style.width = `${width}%`;
+        }, 100)
+    })
 
-function createToast(type) {
-    console.log("Toast notification created", type);
-    const toastDiv = document.createElement("div")
+
+
+}
+
+async function createToast(type) {
+    if (toastDiv) {
+        width = 1;
+        clearInterval(timerId)
+        document.body.removeChild(toastDiv)
+    }
+    toastDiv = document.createElement("div")
     toastDiv.classList.add("toast-notifications")
     toastDiv.innerText = `${icon[type]} ${data[type]}`
 
@@ -30,6 +53,11 @@ function createToast(type) {
     toastDiv.appendChild(progressBar)
 
     document.body.appendChild(toastDiv)
+
+    await runTimer()
+
+    document.body.removeChild(toastDiv)
+    toastDiv = undefined
 }
 
 btnsList.forEach(btn => {
