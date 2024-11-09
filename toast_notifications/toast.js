9329c0d4951c61a3635a27ddb6540cd4f230b627
Gnani-is-Coding <gnanendragariminti@gmail.com>
2024-11-09 19:47:46 +0530

    adds dynamic CSS, slideIN and slideOut animations for toasts

--- toast_notifications/toast.js
+++ toast_notifications/toast.js
@@ -14,20 +14,21 @@ const data = {
     submit: 'Assignment submitted successfully',
     failed: 'Assignment submission failed',
     warning: 'Assignment submission critically failed',
-    information: 'Assignment submission information: failed',
+    information: 'Information about assignment',
 }
 
 const btnsList = document.querySelectorAll(".btn");
 let width = 0;
 let timerId = null
 let toastDiv = undefined;
-function runTimer() {
+function runTimer(toastDiv) {
     return new Promise((resolve, reject) => {
         timerId = setInterval(() => {
             width += 4;
+
             if (width > 100) {
                 clearInterval(timerId)
-                width = 0;
+                width = 1;
                 return resolve()
             }
             document.querySelector(".progress").style.width = `${width}%`;
@@ -44,20 +45,29 @@ async function createToast(type) {
         clearInterval(timerId)
         document.body.removeChild(toastDiv)
     }
+
     toastDiv = document.createElement("div")
-    toastDiv.classList.add("toast-notifications")
-    toastDiv.innerText = `${icon[type]} ${data[type]}`
+    toastDiv.classList.add("toast-notifications", type)
+    toastDiv.innerHTML = `${icon[type]} ${data[type]}`
 
     const progressBar = document.createElement("div")
     progressBar.classList.add("progress")
     toastDiv.appendChild(progressBar)
 
+    toastDiv.style.transform = "translate(- 25px)"
     document.body.appendChild(toastDiv)
 
-    await runTimer()
+    toastDiv.addEventListener("transitionend", () => {
+        if (toastDiv) {
+            document.body.removeChild(toastDiv)
+            toastDiv = undefined
+        }
 
-    document.body.removeChild(toastDiv)
-    toastDiv = undefined
+    })
+
+    await runTimer(toastDiv)
+    toastDiv.style.transform = "translate(calc(100% + 25px))"
+
 }
 
 btnsList.forEach(btn => {
